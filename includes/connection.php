4c0f434ee4869d7c54760f3aa329957f26a7af05
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-15 10:08:42 +1300

    All files moved over from richard's repo and adapted into readable code

---
+++ includes/connection.php
@@ -0,0 +1,11 @@
+<?php
+	$dbc = mysqli_connect(getenv("DB_HOST"), getenv("DB_USER"), getenv("DB_PASS", getenv("DB_TABLE")));
+
+	if($dbc){
+		// var_dump("Connection successful (Code: 200)");
+
+		$dbc->set_charset("utf8");
+	} else {
+		die("Database connection failed");
+	}
+?>
