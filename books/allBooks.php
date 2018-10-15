e113cd969065f83056dbb3ae67f8bed472b690c5
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-16 09:34:15 +1300

    Added code for singular book viewing

--- books/allBooks.php
+++ books/allBooks.php
@@ -1,7 +1,7 @@
 <?php
 	require "../includes/head.php";
 
-	$sql = "SELECT `id`, `book_name`, `image_name` FROM `books` WHERE 1";
+	$sql = "SELECT `id`, `book_name`, `image_name` FROM `books`";
 
 	$result = mysqli_query($dbc, $sql);
 
@@ -30,13 +30,13 @@
 			<?php foreach($allBooks as $book): ?>
 				<div class="col-md-4">
 					<div class="card mb-4 shadow-sm">
-						<img class="card-img-top" src="./img/uploads/thumbs/<?= $book["image_name"]; ?>" alt="Book cover">
+						<img class="card-img-top" src="./img/uploads/thumbs/<?= $book['image_name']; ?>" alt="Book cover">
 						<div class="card-body">
-							<p class="card-text"><?= $book["book_name"]; ?></p>
+							<p class="card-text"><?= $book['book_name']; ?></p>
 							<div class="d-flex justify-content-between align-items-center">
 								<div class="btn-group">
-									<a href="./books/book.php" class="btn btn-sm btn-outline-info">View</a>
-									<a href="./books/update.php" class="btn btn-sm btn-outline-secondary">Edit</a>
+									<a href="./books/book.php?id=<?= $book['id']; ?>" class="btn btn-sm btn-outline-info">View</a>
+									<a href="./books/update.php?id=<?= $book['id']; ?>" class="btn btn-sm btn-outline-secondary">Edit</a>
 								</div>
 							</div>
 						</div>
--- books/book.php
+++ books/book.php
@@ -1,35 +1,47 @@
-<?php require "../includes/head.php"; ?>
+<?php
+	require "../includes/head.php";
+
+	$id = $_GET["id"];
+
+	$sql  = "SELECT * FROM `books` WHERE id = $id";
+
+	$result = mysqli_query($dbc, $sql);
+
+	if($result && mysqli_affected_rows($dbc) > 0){
+		$book = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	} else {
+		die("ERROR: Database SELECT failed");
+	}
+?>
 
 <div class="container">
 	<div class="row mb-2">
 		<div class="col">
-			<h1>Harry Potter and the Philosopher's Stone</h1>
+			<h1><?= $book["book_name"]; ?></h1>
 		</div>
 	</div>
 
 	<div class="row mb-2">
 		<div class="col">
-			<a class="btn btn-outline-primary" href="./books/update.php">Edit</a>
-			<a class="btn btn-outline-danger" href="./books/confirm_delete.php">Delete</a>
+			<a class="btn btn-outline-primary" href="./books/update.php?id=<?= $id ?>">Edit</a>
+			<a class="btn btn-outline-danger" href="./books/confirm_delete.php?id=<?= $id ?>">Delete</a>
 		</div>
 	</div>
 
 	<div class="row mb-2">
 		<div class="col-xs-12 col-sm-4 align-self-center">
-			<img class="img-fluid" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+			<img class="img-fluid" src="./img/uploads/thumbs/<?= $book['image_name']; ?>" alt="Book cover">
 		</div>
 
         <div class="col-xs-12 col-sm-8 align-self-center">
-            <h3>Harry Potter and the Philosopher's Stone</h3>
-            <h4>J.K Rowling</h4>
+            <h3><?= $book["book_name"]; ?></h3>
+            <h4><?= $book["author"]; ?></h4>
         </div>
     </div>
 
     <div class="row mb-2">
         <div class="col-12">
-            <p>
-                Harry Potter has been living an ordinary life, constantly abused by his surly and cold aunt and uncle, Vernon and Petunia Dursley and bullied by their spoiled son Dudley since the death of his parents ten years prior. His life changes on the day of his eleventh birthday when he receives a letter of acceptance into a Hogwarts School of Witchcraft and Wizardry, delivered by a half-giant named Rubeus Hagrid after previous letters had been destroyed by Harry’s Uncle Vernon and his Aunt Petunia. Hagrid explains Harry's hidden past as the wizard son of James and Lily Potter, who were a wizard and witch respectively, and how they were murdered by the most evil and powerful dark wizard of all time, Lord Voldemort, which resulted in the one-year-old Harry being sent to live with his aunt and uncle. The strangest bit of the murder was how Voldemort was unable to kill him, but instead had his own powers removed and blasted away, sparking Harry's immense fame among the magical community.
-            </p>
+            <p><?= $book["description"]; ?></p>
         </div>
     </div>
 </div>
