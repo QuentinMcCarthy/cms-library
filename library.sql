20a1bb305ee758ccecdc66263b19211b8352a1f7
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-17 12:13:21 +1300

    Updated database backup

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 16, 2018 at 08:44 PM
+-- Generation Time: Oct 16, 2018 at 11:12 PM
 -- Server version: 5.7.23-0ubuntu0.16.04.1
 -- PHP Version: 7.2.3-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -43,6 +43,26 @@ INSERT INTO `books` (`id`, `book_name`, `author`, `description`, `image_name`) V
 (2, 'Warrior Cats: Into the Wild', 'Erin Hunter', 'Fire alone can save our Clan...\r\n\r\nFor generations, four Clans of wild cats have shared the forest according to the laws laid down by the powerful ancestors. But the warrior code is threatened, and the ThunderClan cats are in grave danger. The sinister ShadowClan grows stronger every day. Noble warriors are dying - and some deaths are more mysterious than others.\r\n\r\nIn the midst of this turmoil appears an ordinary housecat named Rusty . . . Who may yet turn out to be the bravest warrior of them all.', '5bc640eb5639d.jpg'),
 (3, 'The Spook\'s Apprentice', 'Joseph Delaney', '\'Someone has to stand against the dark. And you\'re the only one who can\'\r\nFor years, the local Spook has been keeping the County safe from evil. Now his time is coming to an end, but who will take over? Many apprentices have tried. Some floundered, some fled, some failed to stay alive. Just one boy is left. Thomas Ward. He is the last hope. But does he stand a chance against Mother Malkin, the most dangerous witch in the County?', '5bc641065b537.jpg');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `userid` tinyint(6) UNSIGNED NOT NULL,
+  `email` varchar(254) CHARACTER SET utf8mb4 NOT NULL,
+  `username` varchar(25) CHARACTER SET utf8mb4 NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userid`, `email`, `username`, `password`) VALUES
+(1, 'qmccarthy97@gmail.com', 'root', '$2y$10$lBtoMfnJwgEnK8GBdnVxmuGrvo.vaVHa7iCWbcCNTegoxl4qna3Q6');
+
 --
 -- Indexes for dumped tables
 --
@@ -55,6 +75,12 @@ ALTER TABLE `books`
   ADD UNIQUE KEY `image_name` (`image_name`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,6 +89,11 @@ ALTER TABLE `books`
 --
 ALTER TABLE `books`
   MODIFY `id` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userid` tinyint(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
