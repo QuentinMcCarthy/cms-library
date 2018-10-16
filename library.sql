9eaf6fc7e5a69dffd5395db7b855b737b2033884
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-17 09:44:58 +1300

    Update database backup

--- library.sql
+++ library.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 16, 2018 at 08:07 PM
+-- Generation Time: Oct 16, 2018 at 08:44 PM
 -- Server version: 5.7.23-0ubuntu0.16.04.1
 -- PHP Version: 7.2.3-1+ubuntu16.04.1+deb.sury.org+1
 
@@ -39,7 +39,7 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `book_name`, `author`, `description`, `image_name`) VALUES
-(1, 'Harry Potter and the Philosopher\'s Stone', 'J.K.Rowling', 'Harry Potter has been living an ordinary life, constantly abused by his surly and cold aunt and uncle, Vernon and Petunia Dursley and bullied by their spoiled son Dudley since the death of his parents ten years prior. His life changes on the day of his eleventh birthday when he receives a letter of acceptance into a Hogwarts School of Witchcraft and Wizardry.', '5bc640c6f0797.jpg'),
+(1, 'Harry Potter and the Philosopher\'s Stone', 'J.K.Rowling', 'Harry Potter has been living an ordinary life, constantly abused by his surly and cold aunt and uncle, Vernon and Petunia Dursley and bullied by their spoiled son Dudley since the death of his parents ten years prior. His life changes on the day of his eleventh birthday when he receives a letter of acceptance into a Hogwarts School of Witchcraft and Wizardry.', '5bc64cccd8e24.jpg'),
 (2, 'Warrior Cats: Into the Wild', 'Erin Hunter', 'Fire alone can save our Clan...\r\n\r\nFor generations, four Clans of wild cats have shared the forest according to the laws laid down by the powerful ancestors. But the warrior code is threatened, and the ThunderClan cats are in grave danger. The sinister ShadowClan grows stronger every day. Noble warriors are dying - and some deaths are more mysterious than others.\r\n\r\nIn the midst of this turmoil appears an ordinary housecat named Rusty . . . Who may yet turn out to be the bravest warrior of them all.', '5bc640eb5639d.jpg'),
 (3, 'The Spook\'s Apprentice', 'Joseph Delaney', '\'Someone has to stand against the dark. And you\'re the only one who can\'\r\nFor years, the local Spook has been keeping the County safe from evil. Now his time is coming to an end, but who will take over? Many apprentices have tried. Some floundered, some fled, some failed to stay alive. Just one boy is left. Thomas Ward. He is the last hope. But does he stand a chance against Mother Malkin, the most dangerous witch in the County?', '5bc641065b537.jpg');
 
