4291a85d1ea288a20ca5632426989cb7fc7979af
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-10-17 08:31:24 +1300

    update.php in progress, however UPDATE query is erronous

--- books/update.php
+++ books/update.php
@@ -1,34 +1,169 @@
-<?php require "../includes/head.php"; ?>
+<?php
+	require "../includes/head.php";
+
+	$id = $_GET['id'];
+
+	$sql = "SELECT * FROM `books` WHERE id = $id";
+
+	$result = mysqli_query($dbc, $sql);
+
+	if($result && mysqli_affected_rows($dbc) > 0){
+		$book = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	} else if($result && mysqli_affected_rows($dbc) == 0){
+		die("Error 404");
+	} else {
+		die("ERROR: Database SELECT failed");
+	}
+
+	if($_POST){
+        extract($_POST);
+
+        $errors = array();
+
+        if(!$title){
+            array_push($errors, "Title is required, please enter a value");
+        } else if(strlen($title) < 2){
+            array_push($errors, "Please enter at least 2 characters for your Title");
+        } else if(strlen($title) > 100){
+            array_push($errors, "The title can't be more than 100 characters");
+        }
+
+        if(!$author){
+            array_push($errors, "An author is required, please enter a value");
+        } else if(strlen($author) < 2){
+            array_push($errors, "Please enter at least 2 characters for the author");
+        } else if(strlen($author) > 100){
+            array_push($errors, "The Author Name can't be more than 100 characters");
+        }
+
+        if(!$description){
+            array_push($errors, "A description is required, please enter a value");
+        } else if(strlen($description) < 10){
+            array_push($errors, "The description must be at least 10 characters long");
+        } else if(strlen($description) > 1000){
+            array_push($errors, "The description needs to be less than 1000 characters");
+        }
+
+        if(isset($_FILES['image'])){
+            $fileSize = $_FILES['image']['size'];
+            $fileTmp = $_FILES['image']['tmp_name'];
+            $fileType = $_FILES['image']['type'];
+
+            if($fileSize == 0){
+				$filename = mysqli_real_escape_string($dbc, $book['image_name']);
+            } else if($fileSize > 5000000){
+                array_push($errors, "The file is to large, must be under 5MB");
+            } else {
+                $validExtensions = array(
+					"jpeg",
+					"jpg",
+					"png"
+				);
+
+                $fileNameArray = explode(".", $_FILES['image']['name']);
+
+                $fileExt = strtolower(end($fileNameArray));
+
+                if(in_array($fileExt, $validExtensions) === false){
+                    array_push($errors, "File type not allowed, can only be a jpg or png");
+                }
+            }
+        }
+
+        if(empty($errors)){
+			$title = mysqli_real_escape_string($dbc, $title);
+			$author = mysqli_real_escape_string($dbc, $author);
+			$description = mysqli_real_escape_string($dbc, $description);
+
+			if(isset($fileExt)){
+	            $newFileName = uniqid().".".$fileExt;
+				$filename = mysqli_real_escape_string($dbc, $newFileName);
+			}
+
+			$sql = "UPDATE `books` SET `book_name`=$title,`author`=$author,`description`=$description,`image_name`=$filename WHERE id = $id";
+
+			die($sql);
+
+			$result = mysqli_query($dbc, $sql);
+
+			if($result && mysqli_affected_rows($dbc) > 0){
+				if(isset($fileExt)){
+		            $destination = "../img/uploads";
+
+		            if(!is_dir($destination)){
+		                mkdir("../img/uploads/", 0777, true);
+		            }
+
+		            // move_uploaded_file($fileTmp, $destination."/".$newFileName);
+
+		            $manager = new ImageManager();
+		            $mainImage = $manager->make($fileTmp);
+		            $mainImage->save($destination."/".$newFileName, 100);
+
+		            $thumbnailImage = $manager->make($fileTmp);
+		            $thumbDestination = "../img/uploads/thumbs";
+
+		            if(!is_dir($thumbDestination)){
+		                mkdir("../img/uploads/thumbs/", 0777, true);
+		            }
+
+		            $thumbnailImage->resize(300, null, function($constraint){
+		                $constraint->aspectRatio();
+		                $constraint->upsize();
+		            });
+
+		            $thumbnailImage->save($thumbDestination."/".$newFileName, 100);
+				}
+
+	            header("Location: book.php?id=$id");
+			} else {
+				die("ERROR: Database UPDATE failed");
+			}
+        }
+    }
+?>
 
 <div class="container">
 	<div class="row mb-2">
 		<div class="col">
-			<h1>Edit Harry Potter and the Philosopher's Stone</h1>
+			<h1>Edit <?= $book['book_name']; ?></h1>
 		</div>
 	</div>
 
+	<?php if($_POST && !empty($errors)): ?>
+		<div class="row mb-2">
+			<div class="col">
+				<div class="alert alert-danger" role="alert">
+					<ul>
+						<?php foreach($errors as $error): ?>
+							<li><?= $error; ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+
 	<div class="row mb-2">
 		<div class="col">
-			<form action="update.php" method="post" enctype="multipart/form-data">
+			<form action="./books/update.php?id=<?= $id; ?>" method="post" enctype="multipart/form-data">
 				<div class="form-group">
 					<label for="title">Book Title</label>
-					<input type="text" class="form-control"  placeholder="Enter book title" value="Harry Potter and the Philosopher's Stone">
+					<input type="text" class="form-control" name="title" placeholder="Enter book title" value="<?= $book['book_name']; ?>">
 				</div>
 
 				<div class="form-group">
 					<label for="author">Author</label>
-					<input type="text" class="form-control"  placeholder="Enter books author" value="J.K Rowling">
+					<input type="text" class="form-control" name="author" placeholder="Enter books author" value="<?= $book['author']; ?>">
 				</div>
 
 				<div class="form-group">
 					<label for="author">Book Description</label>
-					<textarea class="form-control" name="description" rows="8" cols="80" placeholder="Description about the book">
-						Harry Potter has been living an ordinary life, constantly abused by his surly and cold aunt and uncle, Vernon and Petunia Dursley and bullied by their spoiled son Dudley since the death of his parents ten years prior. His life changes on the day of his eleventh birthday when he receives a letter of acceptance into a Hogwarts School of Witchcraft and Wizardrydelivered by a half-giant named Rubeus Hagrid after previous letters had been destroyed by Harry’s Uncle Vernon and his Aunt Petunia. Hagrid explains Harry's hidden past as the wizard son of James and Lily Potter, who were a wizard and witch respectively, and how they were murdered by the most evil and powerful dark wizard of all time, Lord Voldemort, which resulted in the one-year-old Harry being sent to live with his aunt and uncle. The strangest bit of the murder was how Voldemor was unable to kill him, but instead had his own powers removed and blasted away, sparking Harry's immense fame among the magical community
-					</textarea>
+					<textarea class="form-control" name="description" rows="8" cols="80" placeholder="Description about the book"><?= $book['description']; ?></textarea>
 				</div>
 
 				<div class="form-group">
-					<img data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+					<img src="./img/uploads/thumbs/<?= $book['image_name']; ?>" alt="Card image cap">
 					<label for="file">Upload an Image</label>
 					<input type="file" name="image" class="form-control-file">
 				</div>
